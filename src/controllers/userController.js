73829a3f6b610f8a95d4aa1d77dc2f8923a47f6f
Patricio Sarmiento <143901606+Pato525@users.noreply.github.com>
2024-10-17 22:11:28 -0300

    CRUD finalizada

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,31 +6,51 @@ const getAllUsers = async (req, res) => {
 };
 
 const getUserById = async (req, res) => {
-  const user = userService.getUserById(req.params.id);
+  const userId = parseInt(req.params.id, 10);  // Convertimos el id a número
+  const user = await userService.getUserById(userId);  // Buscar el usuario por id
 
+  if (user) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(user));
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
+  }
 };
 
 const createUser = async (req, res) => {
-  const user = userService.createUser(req.body);
-  res.status(201).send(user);
+  const user = await userService.createUser(req.body);
+  
+  // Usando los métodos correctos para `restana`
+  res.writeHead(201, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify(user));
 };
 
 const updateUser = async (req, res) => {
-  const updatedUser = userService.updateUser(req.params.id, req.body);
+  const userId = parseInt(req.params.id, 10);  // Convertimos el id a número
+  const updatedUser = await userService.updateUser(userId, req.body);  // Actualizar usuario con los datos del body
+
   if (updatedUser) {
-    res.send(updatedUser);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(updatedUser));
   } else {
-    res.sendStatus(404);
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User not found' }));
   }
 };
 
 const deleteUser = async (req, res) => {
-  const deletedUser = userService.deleteUser(req.params.id);
+  const userId = parseInt(req.params.id, 10);  // Asegúrate de convertir el id a número
+  const deletedUser = await userService.deleteUser(userId);  // Buscar usuario por id
+  
   if (deletedUser) {
-    res.send(deletedUser);
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User correctly deleted' }));
   } else {
-    res.sendStatus(404);
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'User was not found' }));
   }
 };
+
 
 module.exports = { getAllUsers, getUserById, createUser, updateUser, deleteUser };
